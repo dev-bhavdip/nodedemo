f35727fe84c02675d85ff40b245f96c433a608ca
bhavdip <bhavdip.dodiya@esparkbizmail.com>
2023-05-04 14:38:05 +0530

    data table done

--- datatbl/test.js
+++ datatbl/test.js
@@ -1,6 +1,3 @@
-var jsdom = require("jsdom").jsdom;
-global.$ = require('jquery/dist/jquery')(jsdom().createWindow());
-
-$(document).ready(function() {
-    $('#example').DataTable();
-} );
+$(document).ready(function () {
+  $('#example').DataTable();
+});
